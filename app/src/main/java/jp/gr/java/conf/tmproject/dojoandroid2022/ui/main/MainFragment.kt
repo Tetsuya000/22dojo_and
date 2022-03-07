96594bbd72ab42736c15b2e30095e0f3c8bde549
Tetsuya000 <mt9611elle@gmail.com>
2022-03-07 15:16:42 +0900

    [Add] Added navigation

--- app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/ui/main/MainFragment.kt
+++ app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/ui/main/MainFragment.kt
@@ -1,32 +1,15 @@
 package jp.gr.java.conf.tmproject.dojoandroid2022.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import jp.gr.java.conf.tmproject.dojoandroid2022.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
-    companion object {
-        fun newInstance() = MainFragment()
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
     }
-
-    private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
