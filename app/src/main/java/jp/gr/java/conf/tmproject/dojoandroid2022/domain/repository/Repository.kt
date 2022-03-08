3c79f23ed9887e0cf02c436181fa52c5a1ee856c
Tetsuya000 <mt9611elle@gmail.com>
2022-03-08 19:08:55 +0900

    [Fix] Fixed repository

--- app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/domain/repository/Repository.kt
+++ app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/domain/repository/Repository.kt
@@ -1,6 +1,9 @@
 package jp.gr.java.conf.tmproject.dojoandroid2022.domain.repository
 
+import jp.gr.java.conf.tmproject.dojoandroid2022.domain.repository.model.RoadMapModel
+
 interface Repository {
+    fun parseRodeMap(): RoadMapModel
     fun saveCharacterName(characterName: String)
     fun loadCharacterName(): String
 }
