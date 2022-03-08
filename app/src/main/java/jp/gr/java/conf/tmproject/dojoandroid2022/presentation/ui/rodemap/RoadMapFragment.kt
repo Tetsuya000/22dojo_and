dedfb1c47d213bd183a9135d002726f8f88bd17e
Tetsuya000 <mt9611elle@gmail.com>
2022-03-08 16:06:11 +0900

    [Add] Parse rodemap

--- app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/presentation/ui/rodemap/RoadMapFragment.kt
+++ app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/presentation/ui/rodemap/RoadMapFragment.kt
@@ -5,6 +5,11 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import jp.gr.java.conf.tmproject.dojoandroid2022.R
 import jp.gr.java.conf.tmproject.dojoandroid2022.databinding.RoadmapFragmentBinding
+import jp.gr.java.conf.tmproject.dojoandroid2022.domain.repository.model.RoadMapModel
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 class RoadMapFragment : Fragment(R.layout.roadmap_fragment) {
 
@@ -18,6 +23,16 @@ class RoadMapFragment : Fragment(R.layout.roadmap_fragment) {
         super.onViewCreated(view, savedInstanceState)
 
         _binding = RoadmapFragmentBinding.bind(view)
+        parseRodeMap()
+    }
+
+    private fun parseRodeMap(): RoadMapModel = Json.decodeFromString(getJsonData())
+
+    private fun getJsonData(): String {
+        val assetManager = resources.assets
+        val inputStream = assetManager.open("android-developer-roadmap2022.json")
+        val bufferedReader = BufferedReader(InputStreamReader(inputStream))
+        return bufferedReader.readText()
     }
 
     override fun onDestroyView() {
