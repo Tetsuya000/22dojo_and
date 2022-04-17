fea639273a415af32dce9ad9bf2472b37eab6a93
Tetsuya000 <mt9611elle@gmail.com>
2022-04-17 21:48:37 +0900

    Refactor

--- app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/domain/interactor/GetCharacterLevelUseCaseImpl.kt
+++ app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/domain/interactor/GetCharacterLevelUseCaseImpl.kt
@@ -1,18 +1,18 @@
 package jp.gr.java.conf.tmproject.dojoandroid2022.domain.interactor
 
 import jp.gr.java.conf.tmproject.dojoandroid2022.domain.repository.RoadmapRepository
-import jp.gr.java.conf.tmproject.dojoandroid2022.presentation.util.constants.CharacterSpecification.CHANGE_LEVEL_NUMBER
+import jp.gr.java.conf.tmproject.dojoandroid2022.presentation.util.constants.CharacterConstants.CHANGE_LEVEL_NUMBER
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetCharacterLevelUseCaseImpl @Inject constructor(
-    private val roadmapRepository: RoadmapRepository
-) : GetCharacterLevelUseCase {
+    private val roadmapRepository: RoadmapRepository) : GetCharacterLevelUseCase {
 
     override fun getCharacterLevel(): Flow<Int> {
-        return roadmapRepository.loadAllNode().map { masterNode ->
-            (masterNode.size / CHANGE_LEVEL_NUMBER) + 1
-        }
+        return roadmapRepository.loadAllNode()
+            .map { masterNode ->
+                (masterNode.size / CHANGE_LEVEL_NUMBER) + 1
+            }
     }
 }
--- app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/presentation/util/constants/Constants.kt
+++ app/src/main/java/jp/gr/java/conf/tmproject/dojoandroid2022/presentation/util/constants/Constants.kt
@@ -1,6 +1,6 @@
 package jp.gr.java.conf.tmproject.dojoandroid2022.presentation.util.constants
 
-object CharacterSpecification {
+object CharacterConstants {
     const val CHANGE_LEVEL_NUMBER = 2
     const val CHANGE_IMAGE_NUMBER = 5
 }
